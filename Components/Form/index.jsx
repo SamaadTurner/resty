3f2f841cf334bbb50f186980c0bbf92ee1496379
SamaadTurner <samaad.turner@gmail.com>
2023-10-24 22:18:38 -0700

    phase 2

--- Components/Form/index.jsx
+++ Components/Form/index.jsx
@@ -1,34 +1,43 @@
-//import React from 'react';
+import { useState } from 'react';
+
 import './Form.scss';
 
 const Form = (props) => {
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [method, setMethod] = useState('GET');
+  const [json, setJson] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method: 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: url,
+      json: json,
     };
     props.handleApiCall(formData);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
         <label>
-          <span>URL: </span>
-          <input name="url" type="text" />
-          <button type="submit">GO!</button>
+          <span data-testid="form-span" >URL: </span>
+          <input data-testid="form-input" name='url' type='text' onChange={(e) => setUrl(e.target.value)}/>
+          <button data-testid="form-button" type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <span data-testid="form-get" id="get" onClick={() => setMethod('GET')} style={{ backgroundColor: method === 'GET' ? 'green' : 'grey'}}>GET</span>
+          <span data-testid="form-post" id="post" onClick={() => setMethod('POST')} style={{ backgroundColor: method === 'POST' ? 'green' : 'grey'}}>POST </span>
+          <span data-testid="form-put" id="put" onClick={() => setMethod('PUT')} style={{ backgroundColor: method === 'PUT' ? 'green' : 'grey'}}>PUT</span>
+          <span data-testid="form-delete" id="delete" onClick={() => setMethod('DELETE')} style={{ backgroundColor: method === 'DELETE' ? 'red' : 'grey'}}>DELETE</span>
         </label>
+        {method === 'POST' && <textarea rows="4" cols="35" onChange={(e) => setJson(e.target.value)}/>}
+        {method === 'PUT' && <textarea rows="4" cols="35" onChange={(e) => setJson(e.target.value)}/>}
       </form>
     </>
   );
-};
+}
+
 
 export default Form;
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from '../Components/Header/index.jsx';
+import Footer from '../Components/Footer/index.jsx';
+import Form from '../Components/Form/index.jsx';
+import Results from '../Components/Results/index.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
+  const [loading, setLoading] = useState(false);
 
+  const apiCall = async (requestParams) => {
+    setLoading(true);
+    const data = {
+      count: 2,
+      results: [
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+      ],
+
+    };
+    setData(data);
+    setLoading(false);
+    setRequestParams(requestParams);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+         <Header />
+      <div data-testid="app-method" className="divvy">Request Method: {requestParams.method}</div>
+      <div data-testid="app-url" className="divvy">URL: {requestParams.url}</div>
+      { 
+        requestParams.json
+        ? <div className="divvy">Sent JSON: {requestParams.json}</div>
+        : <div></div>
+      }
+      <Form handleApiCall={apiCall} />
+      <Results data={data} loading={loading} />
+      <Footer />
+ 
     </>
-  )
+  );
 }
 
 export default App
